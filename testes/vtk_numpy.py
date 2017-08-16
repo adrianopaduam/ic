1ae7846ac9014c32380cb57bb4e7f1f6d06ef7e3
Adriano Paduam de Souza <adriano.souza.1990@gmail.com>
2017-08-16 02:36:30 -0300

    Otimização Script vtk_numpy

--- testes/vtk_numpy.py
+++ testes/vtk_numpy.py
@@ -1,29 +1,26 @@
-import vtk
-import numpy as np
+import numpy
+import vtk as vt
 import vtkgdcm as gdcm
 from vtk.util.numpy_support import vtk_to_numpy
 from matplotlib import pyplot as plt
 
 class vtk_numpy():
 	def __init__(self):
-		reader = gdcm.vtkGDCMImageReader()
-		imagem = np.empty(0)
+		vtk = gdcm.vtkGDCMImageReader()
+		np = numpy.empty(0)		
 
-	def le_arquivo(self, arquivo):
-		self.reader.SetFileName(arquivo)
-		self.reader.Update()
+	def transformar(self, arquivo):
+		self.vtk.SetFileName(arquivo)
+		self.vtk.Update()
+		self.np = vtk_to_numpy(vtk.GetOutput().GetPointData().GetArray(0))
+		self.np.shape = (512, 512)
 
-	def transforma_arquivo(self):
-		self.imagem = vtk_to_numpy(reader.GetOutput().GetPointData().GetArray(0))
-		self.imagem.shape = (512, 512)
-
-	def imprime_imagem(self):
-		plt.imshow(self.imagem, cmap = "Greys_r")
+	def imprimir(self):
+		plt.imshow(self.np, cmap = "Greys_r")
 		plt.show()
 
 if __name__ == "__main__":
 	import sys
 	obj = vtk_numpy()
-	obj.le_arquivo(sys.argv[1])
-	obj.transforma_arquivo()
-	obj.imprime_imagem()
+	obj.transformar(sys.argv[1])
+	obj.imprimir()
